91a9316f3a8e08b35a6e204191be43bdb4b4a5cb
amankumar-srivastava <amankumar-srivastava@hcl.com>
2023-05-09 13:19:05 +0530

    springboot project commit

---
+++ src/main/java/com/hcl/fsc/FreshSuperChargersApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.fsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshSuperChargersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshSuperChargersApplication.class, args);
+	}
+
+}
