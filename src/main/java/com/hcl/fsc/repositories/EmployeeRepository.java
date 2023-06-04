5c824c24bcb847d6f2ffb4d273f39633a349636e
Vishiv-Panchal <vishiv.panchal@hcl.com>
2023-06-04 11:20:47 +0530

    Controller Added for Excel Sheets

--- src/main/java/com/hcl/fsc/repositories/EmployeeRepository.java
+++ src/main/java/com/hcl/fsc/repositories/EmployeeRepository.java
@@ -1,14 +1,13 @@
 package com.hcl.fsc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import com.hcl.fsc.entities.Employee;
+import com.hcl.fsc.entities.EmployeeDetails;
 
 
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<EmployeeDetails, Long>{
 	
 }
